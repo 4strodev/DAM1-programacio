01ebc9b0dcfa64c864a2a0bfd8f42fb4951dd435
4strodev <jumarurex@gmail.com>
2022-09-28 16:06:33 +0200

    initial commit

---
+++ uf1/hola_mon/Car.java
@@ -0,0 +1,8 @@
+package main
+
+public class Car {
+    int price;
+    public Car(int price) {
+        this.price = price;
+    }
+}
---
+++ uf1/hola_mon/HolaMon.java
@@ -0,0 +1,7 @@
+package main
+
+public class HolaMon {
+    public static void main(String[] args) {
+        System.out.println("Hola, món!");
+    }
+}
